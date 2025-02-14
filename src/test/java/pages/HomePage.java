3746e395a5cb7a70d953b460af58270441f98897
Rakesh R Thampi <rakesh.r.thampi@gmail.com>
2025-02-14 14:58:30 +0530

    Maven compiler and surefier plugin addded and tested using mvn commands

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,47 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import base.TestBase;
+
+public class HomePage extends TestBase {
 	private static WebDriver driver;
+	private List<WebElement> dropElems;
+	private Select objSelect;
+	private final By sortDrop = By.xpath("//select[@class='product_sort_container']");
 
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
+	}
+
+	public int checkDropdownCount() {
+		System.out.println("HomePage::checkDropdown()");
+		waitForElementBy(sortDrop);
+		objSelect = new Select(driver.findElement(sortDrop));
+		dropElems = objSelect.getOptions();
+		System.out.println("HomePage::checkDropdown() - Dropdown item count:" + dropElems.size());
+		return dropElems.size();
+	}
+
+	public boolean sortDropDownByOption(Integer sortOption) {
+		System.out.println("HomePage::sortDropDownByOption() - Sort option = " + sortOption);
+		waitForElementBy(sortDrop);
+		try {
+			// RRT:: Check 'SelectByValue' and update data provider accordingly. Inspect why it fails
+			objSelect.selectByIndex(sortOption);
+		} catch (Exception e) {
+			System.out.println("HomePage::sortDropDownByOption() - EXCEPTION!");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,5 +1,34 @@
 package tests;
 
-public class HomePageTest {
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
+import base.TestBase;
+import pages.HomePage;
+
+public class HomePageTest extends TestBase {
+	HomePage home;
+
+	@Test(priority = 1)
+	@Parameters({ "dropDownCount" })
+	public void checkDropDownItems(String optionCount) {
+		System.out.println("HomePageTest::checkDropDownItems() Test start");
+		home = new HomePage(driver);
+		int expCount = Integer.parseInt(optionCount);
+		Assert.assertEquals(home.checkDropdownCount(), expCount);
+		System.out.println("HomePageTest::checkDropDownItems() Test end");
+	}
+
+	@Test(priority = 2, dataProvider = "sortOptions")
+	public void sortDropDownFromDataProviderOptions(Integer option) {
+		System.out.println("HomePageTest::checkDropDownItems() Test start - Which option to sort = " + option);
+		home.sortDropDownByOption(option);
+	}
+
+	@DataProvider(name = "sortOptions")
+	public Integer[] getDropDownOptions() {
+		return new Integer[] { 0, 1, 2, 3 };
+	}
 }
